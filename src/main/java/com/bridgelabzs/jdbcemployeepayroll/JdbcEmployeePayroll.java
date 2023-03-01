866fa17aece31b6c0bc0ab15d9505547274e5326
sumi <sumipavib1302@gmail.com>
2023-03-01 21:54:26 +0530

    Added:Ability for Employee Payroll Service to retrieve the Employee Payroll from the Database

--- src/main/java/com/bridgelabzs/jdbcemployeepayroll/JdbcEmployeePayroll.java
+++ src/main/java/com/bridgelabzs/jdbcemployeepayroll/JdbcEmployeePayroll.java
@@ -10,17 +10,13 @@ public class JdbcEmployeePayroll {
             // Create a connection to the database
             Connection conn = DriverManager.getConnection(
             		"jdbc:mysql://localhost:3306/employee_Payroll_Service","root","Sumi@1234");
-
             // Create a statement
             Statement stmt = conn.createStatement();
 
             // Execute a query
             ResultSet rs = stmt.executeQuery("SELECT * FROM Employee");
 
-            // Process the result set
-            while (rs.next()) {
-                System.out.println(rs.getString("salary"));
-            }
+            displayResultSet(rs);
 
             // Close the connection
             conn.close();
@@ -29,8 +25,19 @@ public class JdbcEmployeePayroll {
             System.out.println(e.getMessage());
         }
     }
+    public static void displayResultSet(ResultSet rs) throws SQLException {
+        ResultSetMetaData rsmd = rs.getMetaData();
+        int columnsNumber = rsmd.getColumnCount();
+        while (rs.next()) {
+            for (int i = 1; i <= columnsNumber; i++) {
+                if (i > 1) System.out.print(",  ");
+                String columnValue = rs.getString(i);
+                System.out.print(rsmd.getColumnName(i) + " " + columnValue);
+            }
+            System.out.println("");
+        }
+    }
 
 }
-
 
 
